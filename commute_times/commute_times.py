b9a83f0b3186f327a43b1426eef3b776bb43134d
madrury <matthew.drury.83@gmail.com>
2018-07-24 15:31:36 -0700

    Cleanup code a bit.

--- commute_times/commute_times.py
+++ commute_times/commute_times.py
@@ -16,7 +16,7 @@ from commute_times.config import (
     MIDDAY_TOD_FACTOR,
     MORNING_PEAK_TOD_FACTOR,
     EVENING_PEAK_TOD_FACTOR,
-    COMMUTE_TYPE_PROBABILITIES,
+    COMMUTE_TIME_PROBABILITIES,
     CONJESTION_FACTOR_MODE,
     CONJESTION_FACTOR_SHAPE,
     CONJESTION_FACTOR_MINIMUM,
@@ -28,34 +28,7 @@ from commute_times.config import (
     FIRST_OR_FOURTH_TO_OTHER_QUAD_FACTOR_MINIMUM,
 )
 
-
-def make_interval(t, begin, end):
-    return (t >= begin) & (t <= end)
-
-def compute_time_of_day_factor(t):
-    cond_list = [
-        make_interval(t, MORNING_COMMUTE_BEGIN, MORNING_COMMUTE_MID),
-        make_interval(t, MORNING_COMMUTE_MID, MORNING_COMMUTE_END),
-        make_interval(t, MORNING_COMMUTE_END, EVENING_COMMUTE_BEGIN),
-        make_interval(t, EVENING_COMMUTE_BEGIN, EVENING_COMMUTE_MID),
-        make_interval(t, EVENING_COMMUTE_MID, EVENING_COMMUTE_END)
-    ]
-    choice_list = [
-        line_segment(t, MORNING_COMMUTE_BEGIN, NIGHTIME_TOD_FACTOR, 
-                        MORNING_COMMUTE_MID, MORNING_PEAK_TOD_FACTOR),
-        line_segment(t, MORNING_COMMUTE_MID, MORNING_PEAK_TOD_FACTOR, 
-                        MORNING_COMMUTE_END, MIDDAY_TOD_FACTOR),
-        MIDDAY_TOD_FACTOR,
-        line_segment(t, EVENING_COMMUTE_BEGIN, MIDDAY_TOD_FACTOR,
-                        EVENING_COMMUTE_MID, EVENING_PEAK_TOD_FACTOR),
-        line_segment(t, EVENING_COMMUTE_MID, EVENING_PEAK_TOD_FACTOR,
-                        EVENING_COMMUTE_END, NIGHTIME_TOD_FACTOR)
-    ]
-    return np.select(cond_list, choice_list, default=NIGHTIME_TOD_FACTOR)
-
 def sample_time_of_day(n):
-    # Dead-of-night, daytime commute, mid-day, evening commute
-    p_commute_type = COMMUTE_TYPE_PROBABILITIES
     dead_of_night_samples = np.random.uniform(
         EVENING_COMMUTE_END, 24.0 + MORNING_COMMUTE_BEGIN, size=n) % 24.0
     daytime_commute_samples = np.random.triangular(
@@ -71,7 +44,10 @@ def sample_time_of_day(n):
         right=EVENING_COMMUTE_END,
         size=n)
     return np.choose(
-        np.random.choice(4, size=n, p=p_commute_type),
+        np.random.choice(
+            len(COMMUTE_TIME_PROBABILITIES),
+            size=n, 
+            p=COMMUTE_TIME_PROBABILITIES),
         [
             dead_of_night_samples,
             daytime_commute_samples,
@@ -87,12 +63,7 @@ def compute_commute_time(sources, targets, time_of_day):
     N = sources.shape[0]
     raw_distance = compute_raw_distance(sources, targets)
     # Scale is chosen so that the mode of the conjestion_factor is one.
-    conjestion_factor = np.maximum(
-        sample_gamma(
-            n=N, 
-            mode=CONJESTION_FACTOR_MODE, 
-            shape=CONJESTION_FACTOR_SHAPE), 
-        CONJESTION_FACTOR_MINIMUM)
+    conjestion_factor = compute_conjestion_factor(N)
     geometry_factor = compute_geometry_factor(sources, targets)
     time_of_day_factor = compute_time_of_day_factor(time_of_day)
     return (
@@ -100,6 +71,13 @@ def compute_commute_time(sources, targets, time_of_day):
             * geometry_factor 
             * time_of_day_factor 
             * raw_distance)
+
+def compute_conjestion_factor(n):
+    return np.maximum(sample_gamma(
+        n=n, 
+        mode=CONJESTION_FACTOR_MODE, 
+        shape=CONJESTION_FACTOR_SHAPE), 
+    CONJESTION_FACTOR_MINIMUM)
 
 def compute_geometry_factor(sources, targets):
     N = sources.shape[0]
@@ -131,3 +109,24 @@ def compute_geometry_factor(sources, targets):
     ]
     return np.maximum(
         np.select(cond_list, choice_list, default=1.0), 1.0)
+
+def compute_time_of_day_factor(t):
+    cond_list = [
+        make_interval(t, MORNING_COMMUTE_BEGIN, MORNING_COMMUTE_MID),
+        make_interval(t, MORNING_COMMUTE_MID, MORNING_COMMUTE_END),
+        make_interval(t, MORNING_COMMUTE_END, EVENING_COMMUTE_BEGIN),
+        make_interval(t, EVENING_COMMUTE_BEGIN, EVENING_COMMUTE_MID),
+        make_interval(t, EVENING_COMMUTE_MID, EVENING_COMMUTE_END)
+    ]
+    choice_list = [
+        line_segment(t, MORNING_COMMUTE_BEGIN, NIGHTIME_TOD_FACTOR, 
+                        MORNING_COMMUTE_MID, MORNING_PEAK_TOD_FACTOR),
+        line_segment(t, MORNING_COMMUTE_MID, MORNING_PEAK_TOD_FACTOR, 
+                        MORNING_COMMUTE_END, MIDDAY_TOD_FACTOR),
+        MIDDAY_TOD_FACTOR,
+        line_segment(t, EVENING_COMMUTE_BEGIN, MIDDAY_TOD_FACTOR,
+                        EVENING_COMMUTE_MID, EVENING_PEAK_TOD_FACTOR),
+        line_segment(t, EVENING_COMMUTE_MID, EVENING_PEAK_TOD_FACTOR,
+                        EVENING_COMMUTE_END, NIGHTIME_TOD_FACTOR)
+    ]
+    return np.select(cond_list, choice_list, default=NIGHTIME_TOD_FACTOR)
